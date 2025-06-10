269a80bf60068a2fe49974c712c122dcc4626b44
a1rzayev <asifrzayev2006@gmail.com>
2025-06-10 20:09:56 +0400

    Fix forms for auth(login/register)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import LoginForm from '../components/LoginPage/LoginForm';
+import { Container, Card } from 'react-bootstrap';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="container">
-      <LoginForm />
-    </div>
+    <Container className="py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          <Card className="shadow-sm">
+            <Card.Body className="p-4">
+              <h2 className="text-center mb-4">Login</h2>
+              <LoginForm />
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </Container>
   );
 };
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import RegisterForm from '../components/RegisterPage/RegisterForm';
+import { Container, Card } from 'react-bootstrap';
 
 const RegisterPage: React.FC = () => {
   return (
-    <div className="container">
-      <RegisterForm />
-    </div>
+    <Container className="py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          <Card className="shadow-sm">
+            <Card.Body className="p-4">
+              <h2 className="text-center mb-4">Register</h2>
+              <RegisterForm />
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </Container>
   );
 };
 

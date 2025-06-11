afbf571124e432a6cb8e4fb3ad90fc7698579205
a1rzayev <asifrzayev2006@gmail.com>
2025-06-11 15:47:26 +0400

    Make auth pages like in figma

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/LandingPage/Hero';
 import PromoSection from '../components/LandingPage/PromoSection';
 import GroupSelector from '../components/LandingPage/GroupSelector';
-import CourseSection from '../components/LandingPage/CourseSection';
 import PricingSection from '../components/LandingPage/PricingSection';
 import FAQSection from '../components/LandingPage/FAQSection';
 import Footer from '../components/LandingPage/Footer';
@@ -13,7 +12,6 @@ const LandingPage: React.FC = () => {
       <Hero />
       <PromoSection />
       <GroupSelector />
-      <CourseSection />
       <PricingSection />
       <FAQSection />
       <Footer />

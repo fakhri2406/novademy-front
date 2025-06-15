b891ed2c371f3d7c5f185eae451088e565a5043c
a1rzayev <asifrzayev2006@gmail.com>
2025-06-15 04:09:22 +0400

    Fix button styles

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import RegisterForm from '../components/RegisterPage/RegisterForm';
 import '../styles/LoginPage.css';
+import { useTranslation } from '../i18n/useTranslation';
 
 const RegisterPage: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <div className="login-bg">
       <div className="login-card">
-        <h2 className="login-title">Qeydiyyat</h2>
+        <h2 className="login-title">{t('register')}</h2>
         <RegisterForm />
       </div>
     </div>
